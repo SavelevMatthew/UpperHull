d59be0ae71f51a3da6593633bb8f00028f4d8637
matthewsavelev <savelevmatthew@gmail.com>
2020-05-15 17:25:16 +0500

    week 3

--- upperHull.py
+++ upperHull.py
@@ -20,23 +20,25 @@ def get_upper_convex_hull(G_grid, psi_grid, dir_grid_ann):
 
 
 def get_min(m, G_grid, psi_grid, dir_grid):
-    print(m)
     alpha = 0.5
     start_point = randint(0, len(dir_grid) - 1)
     last_point = start_point
     last_grad = 0
+    info = "{}/{}".format(m, len(G_grid))
     while True:
-        grad = get_inner_gradient(m, start_point, alpha, G_grid, psi_grid, dir_grid)
-        if abs(grad) < 0.01 or (start_point == 0 and grad > 0) or (start_point == len(dir_grid) - 1 and grad < 0):
+        grad = get_inner_grad(m, start_point, alpha, G_grid, psi_grid, dir_grid)
+        if (start_point == 0 and grad > 0) or (start_point == len(dir_grid) - 1 and grad < 0):
+            print(info + ' ex1 ' + start_point.__str__())
             return get_body_value(m, start_point, G_grid, psi_grid, dir_grid)
-        elif grad * last_grad < 0:
-            return min([get_body_value(m, start_point, G_grid, psi_grid, dir_grid), get_body_value(m, last_point, G_grid, psi_grid, dir_grid)])
+        if grad * last_grad < 0:
+            print(info + ' ex2')
+            return min([get_body_value(m, d, G_grid, psi_grid, dir_grid) for d in [start_point, last_point]])
         last_point = start_point
+        last_grad = grad
         if grad > 0:
             start_point -= 1
         else:
             start_point += 1
-        last_grad = grad
 
     # return min([get_body_value(m, d, G_grid, psi_grid, dir_grid) for d in range(len(dir_grid))])
 
@@ -52,26 +54,28 @@ def get_body_value(m, d, G_grid, psi_grid, dir_grid):
 def get_inner_value(d, g, G_grid, psi_grid, dir_grid):
     return psi_grid[g] + sum(dir_grid[d] * G_grid[g])
 
-
-def get_inner_gradient(m, d, alpha, G_grid, psi_grid, dir_grid):
-    max_values = [get_inner_value(d, g, G_grid, psi_grid, dir_grid) for g in range(len(G_grid))]
-    smooth_grad = smooth_max_grad(max_values, alpha)
-    return sum([smooth_grad[i] * G_grid[i] for i in range(len(smooth_grad))]) - m
+# Smooth max region
 
 
-# @lru_cache()
-def smooth_max(objects, alpha):
-    exps = get_exp(objects, alpha)
-    numerator = sum([objects[i] * exps[i] for i in range(len(objects))])
-    denominator = sum(exps)
+def get_exponents(objects, alpha):
+    return [math.e ** (alpha * obj) for obj in objects]
+
+
+def smooth_max(objects, exponents):
+    numerator = sum([objects[i] * exponents[i] for i in range(len(objects))])
+    denominator = sum([exp for exp in exponents])
     return numerator / denominator
 
 
-def smooth_max_grad(objects, alpha):
-    exps = get_exp(objects, alpha)
-    return [(exps[i] / sum(exps)) * (1 + alpha * (objects[i] - smooth_max(objects, alpha))) for i in range(len(objects))]
+def smooth_max_grad(index, objects, alpha):
+    exps = get_exponents(objects, alpha)
+    sm_max = smooth_max(objects, exps)
+    return (exps[index] / sum([exp for exp in exps])) * (1 + alpha * (objects[index] - sm_max))
+
+# grad region
 
 
-# @lru_cache()
-def get_exp(objects, alpha):
-    return [math.e ** (x * alpha) for x in objects]
+def get_inner_grad(m, d, alpha, G_grid, psi_grid, dir_grid):
+    inner_values = [get_inner_value(d, g, G_grid, psi_grid, dir_grid) for g in range(len(G_grid))]
+    smooth_grad_values = [smooth_max_grad(i, inner_values, alpha) * G_grid[i] for i in range(len(inner_values))]
+    return sum([grad for grad in smooth_grad_values]) - G_grid[m]
