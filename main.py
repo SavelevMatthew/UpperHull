a34fac0441599443723485b63d34d1b8ced56c5f
matthewsavelev <savelevmatthew@gmail.com>
2020-06-05 13:43:08 +0500

    Поддрежка n-мерных пространств

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import matplotlib.pyplot as plt
 from grids import *
 from upperHull import get_upper_convex_hull
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def plot_3d(G_grid, f_grid, G_grid_ann):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(G_grid[:, 0].reshape(G_grid_ann, G_grid_ann),
+                    G_grid[:, 1].reshape(G_grid_ann, G_grid_ann),
+                    f_grid.reshape(G_grid_ann, G_grid_ann))
+
 
 def main():
     builder = GridBuilder(dim=1, ann=100)
@@ -8,12 +18,24 @@ def main():
 
     psi_grid = np.array([np.sin(4 * G_grid[i]) for i in range(len(G_grid))])
 
-    phi_grid = get_upper_convex_hull(G_grid, psi_grid)
+    phi_grid = get_upper_convex_hull(builder, G_grid, psi_grid)
 
     plt.plot(G_grid, psi_grid)
     plt.plot(G_grid, phi_grid)
     plt.show()
 
 
+def main_3d():
+    builder = GridBuilder(2, 10)
+    G_grid = builder.get_circle_grid(1)
+    psi_grid = np.array([np.sin(6 * G_grid[i][0]) * np.cos(4 * G_grid[i][1])
+                         for i in range(len(G_grid))])
+    phi_grid = get_upper_convex_hull(builder, G_grid, psi_grid)
+
+    plot_3d(G_grid, psi_grid, builder.ann)
+    plot_3d(G_grid, phi_grid, builder.ann)
+    plt.show()
+
+
 if __name__ == '__main__':
-    main()
+    main_3d()
--- upperHull.py
+++ upperHull.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
-from functools import lru_cache
 from random import randint
+from collections import deque
 
 
 def get_upper_convex_hull(builder, G_grid, psi_grid):
@@ -9,43 +9,64 @@ def get_upper_convex_hull(builder, G_grid, psi_grid):
     The function make the upper convex hull function of the function psi
     :param G_grid: numpy array of notes of G grid
     :param psi_grid: numpy array of psi values
-    :return: numpy array of phi values that is the upper convex hull function of the function psi
+    :return: numpy array of phi values that is the upper convex hull function
+    of the function psi
     """
     dir_grid = builder.get_directions_grid()
     print(len(G_grid))
-    return np.array([get_min(m, G_grid, psi_grid, dir_grid, builder) for m in range(len(G_grid))])
+    return np.array([get_min(m, G_grid, psi_grid, dir_grid, builder) for m in
+                     range(len(G_grid))])
 
 
 def get_min(m, G_grid, psi_grid, dir_grid, builder):
     alpha = 10
     start_point = randint(0, len(dir_grid) - 1)
     last_point = start_point
-    last_grad = 0
     info = "{}/{}".format(m, len(G_grid))
+    cache = deque(maxlen=int(2**builder.dim))
     while True:
-        grads = list(get_all_inner_grads(m, start_point, alpha, G_grid, psi_grid, dir_grid))
-        if (start_point == 0 and grads[0][0] < 0) or (start_point == len(dir_grid) - 1 and grads[0][0] > 0):
-            print(info + ' ex1 ' + start_point.__str__())
+        grads = list(get_all_inner_grads(m, start_point, alpha, G_grid,
+                                         psi_grid, dir_grid))
+        new_index = get_step(grads, start_point, last_point, builder)
+        if new_index is None:
+            print(info + ' Stable exit ' + start_point.__str__())
             return get_body_value(m, start_point, G_grid, psi_grid, dir_grid)
-        if grads[0] * last_grad < 0:
-            print(info + ' ex2')
-            return min([get_body_value(m, d, G_grid, psi_grid, dir_grid) for d in [start_point, last_point]])
+        elif new_index in cache:
+            print(info + ' Cycle detected!')
+            return min([get_body_value(m, index, G_grid, psi_grid, dir_grid)
+                        for index in cache])
         last_point = start_point
-        last_grad = grads[0]
-        if grads[0][0] > 0:
-            start_point += 1
-        else:
-            start_point -= 1
+        start_point = new_index
+        cache.append(new_index)
     # Старая и рабочая версия, разбитая на подфункции
-    # return min([get_body_value(m, d, G_grid, psi_grid, dir_grid) for d in range(len(dir_grid))])
+    # return min([get_body_value(m, d, G_grid, psi_grid, dir_grid)
+    # for d in range(len(dir_grid))])
+
+
+def get_step(grads, current_index, last_index, builder):
+    pairs = [(i, abs(grads[i])) for i in range(len(grads))]
+    ordered = sorted(pairs, key=lambda x: -x[1])
+    for move in ordered:
+        dim = move[0]
+        grad = grads[dim]
+        new_index = current_index + math.copysign(builder.ann ** dim, grad)
+        new_index = int(new_index)
+        if (new_index == last_index or new_index < 0 or
+                new_index >= builder.ann ** builder.dim or move[1] < 0.05):
+            continue
+        # print(new_index, grad)
+        return new_index
+    return None
 
 
 def get_max(d, G_grid, psi_grid, dir_grid):
-    return max([get_inner_value(d, g, G_grid, psi_grid, dir_grid) for g in range(len(G_grid))])
+    return max([get_inner_value(d, g, G_grid, psi_grid, dir_grid)
+                for g in range(len(G_grid))])
 
 
 def get_body_value(m, d, G_grid, psi_grid, dir_grid):
-    return get_max(d, G_grid, psi_grid, dir_grid) - sum(dir_grid[d] * G_grid[m])
+    return (get_max(d, G_grid, psi_grid, dir_grid)
+            - sum(dir_grid[d] * G_grid[m]))
 
 
 def get_inner_value(d, g, G_grid, psi_grid, dir_grid):
@@ -94,18 +115,31 @@ def smooth_max_grad(index, objects, alpha):
 def get_inner_grad(m, d, alpha, G_grid, psi_grid, dir_grid):
     # Получили множество значений внутренних скобок
     # все, что внутри max в оригинальной формуле
-    inner_values = [get_inner_value(d, g, G_grid, psi_grid, dir_grid) for g in range(len(G_grid))]
+    inner_values = [get_inner_value(d, g, G_grid, psi_grid, dir_grid)
+                    for g in range(len(G_grid))]
     # Для каждого значения ищем градиент и домножаем на mki,
     # Что есть mk в одномерном случае
-    smooth_grad_values = [smooth_max_grad(i, inner_values, alpha) * G_grid[i][0] for i in range(len(inner_values))]
+    smooth_grad_values = [smooth_max_grad(i, inner_values, alpha)
+                          * G_grid[i][0]
+                          for i in range(len(inner_values))]
     # Возвращаем сумму - mi, что просто m в одномерном случае
     return sum([grad for grad in smooth_grad_values]) - G_grid[m][0]
 
 
 def get_all_inner_grads(m, d, alpha, G_grid, psi_grid, dir_grid):
+    """
+    :param m: номер точки расчета
+    :param d: номер угла поворота
+    :param alpha: коэффициент сглаживания
+    :param G_grid: сетка значений параметров
+    :param psi_grid: сетка значений функции
+    :param dir_grid: сетка направлений
+    :return: возвращает градиенты по всем координатам,
+    аналогично get_inner_grad
+    """
     if len(G_grid) == 0:
         return []
-    dimensions = len(G_grid[0])
+    dimensions = G_grid.shape[1]
     inner_values = [get_inner_value(d, g, G_grid, psi_grid, dir_grid) for g in
                     range(len(G_grid))]
     for dim in range(dimensions):
@@ -113,6 +147,3 @@ def get_all_inner_grads(m, d, alpha, G_grid, psi_grid, dir_grid):
             smooth_max_grad(i, inner_values, alpha) * G_grid[i][dim] for i in
             range(len(inner_values))]
         yield sum([grad for grad in smooth_grad_values]) - G_grid[m][dim]
-
-
-
